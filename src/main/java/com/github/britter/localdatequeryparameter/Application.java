7db20b6fbc572194c79e158c674898749b766a3f
Benedikt Ritter <beneritter@gmail.com>
2017-08-04 15:52:09 +0200

    Use a Formatter<LocalDate> for configuring the parsing behavior for LocalDate instances globally

--- src/main/java/com/github/britter/localdatequeryparameter/Application.java
+++ src/main/java/com/github/britter/localdatequeryparameter/Application.java
@@ -15,13 +15,34 @@
  */
 package com.github.britter.localdatequeryparameter;
 
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.format.Formatter;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public Formatter<LocalDate> localDateFormatter() {
+    return new Formatter<LocalDate>() {
+      @Override
+      public LocalDate parse(String text, Locale locale) throws ParseException {
+        return LocalDate.parse(text, DateTimeFormatter.ISO_DATE);
+      }
+
+      @Override
+      public String print(LocalDate object, Locale locale) {
+        return DateTimeFormatter.ISO_DATE.format(object);
+      }
+    };
   }
 }
